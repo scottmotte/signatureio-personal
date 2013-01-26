13096e3e14171e04173783b90de4fff69d56ba81
scottmotte <scott@scottmotte.com>
2013-01-26 14:21:34 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SignatureioPersonal::Application.config.secret_token = '6f2e9f5499c6aa14a28efc7c1d3b5611adbfa07e02f0e2e8adedc3e7ae0b05169cb476ab9b9f2782a9ae5f9804fa2d14f84ce8f1ce37a5010f848735a851f740'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SignatureioPersonal::Application.config.session_store :cookie_store, key: '_signatureio-personal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SignatureioPersonal::Application.config.session_store :active_record_store
